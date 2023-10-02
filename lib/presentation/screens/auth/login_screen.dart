dad9ffe1cc3a5a7357f5eea29761d00c435964dd
Abraar117 <kaziabraar@gmail.com>
2023-10-02 15:32:17 +0530

    login_signup

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+
+  final emailController = new TextEditingController();
+  final passwordController = new TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +22,79 @@ class LoginScreen extends StatelessWidget {
         title: const Text("CargoLinker"),
         centerTitle: true,
       ),
-      body: const Placeholder(),
+      body: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(7)
+                    ),
+                    labelText: 'Email',
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter an email';
+                    }
+                    else if (!EmailValidator.validate(value.trim())) {
+                    return "Invalid email address";
+                  }
+                    return null;
+                  },
+                ),
+
+                SizedBox(height: 16.0,),
+
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(7)
+                  ),
+                    labelText: 'Password',
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a password';
+                    }
+                    return null;
+                  },
+                ),
+                
+                
+                SizedBox(height: 32),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                      
+                      }
+                    },
+                    child: Text('Login'),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account?"),
+                    SizedBox(width: 12,),
+                    TextButton(onPressed: (){},
+                    child: 
+                    Text("Signup"))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
     );
   }
 }
--- lib/presentation/screens/auth/signup_screen.dart
+++ lib/presentation/screens/auth/signup_screen.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 
-class SignupScreen extends StatelessWidget {
-  const SignupScreen({super.key});
+class SignupScreen extends StatefulWidget {
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final emailController = new TextEditingController();
+  final passwordController = new TextEditingController();
+  final repasswordController = new TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +21,99 @@ class SignupScreen extends StatelessWidget {
         title: const Text("CargoLinker"),
         centerTitle: true,
       ),
-      body: const Placeholder(),
+      body: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(7)
+                    ),
+                    labelText: 'Email',
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter an email';
+                    }
+                    else if (!EmailValidator.validate(value.trim())) {
+                    return "Invalid email address";
+                  }
+                    return null;
+                  },
+                ),
+
+                SizedBox(height: 16.0,),
+
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(7)
+                  ),
+                    labelText: 'Password',
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a password';
+                    }
+                    return null;
+                  },
+                ),
+                
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: repasswordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(7)
+                  ),
+                    labelText: 'Re-type Password',
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please re-enter password';
+                    }
+                    else if (value != passwordController.value) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  },
+                ),
+                
+                SizedBox(height: 32),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                      
+                      }
+                    },
+                    child: Text('Sign Up'),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Already have an account?"),
+                    SizedBox(width: 12,),
+                    TextButton(onPressed: (){},
+                    child: 
+                    Text("Login"))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
     );
   }
 }
